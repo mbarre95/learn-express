05bf668e9e91dab70ebdeb98eac4d77094837c0f
Mo Barre <mbarre95@gmail.com>
2024-02-29 18:54:30 -0500

    Feb 29 updates

--- quiz/server.js
+++ quiz/server.js
@@ -29,12 +29,24 @@ app.use(
   cors({origin: 'http://localhost:3000'})
 );
 app.use('/read/usernames', addMsgToRequest);
-
-app.get('/read/usernames', (req, res) => {
-  let usernames = req.users.map(function(user) {
-    return {id: user.id, username: user.username};
+app.get('/read/username/:name', (req, res) => {
+  let name = req.params.name;
+  let users_with_name = req.users.filter(function(user) {
+    return user.username === name;
   });
-  res.send(usernames);
+  console.log(users_with_name);
+  if (users_with_name.length === 0) {
+    res.send({
+      error: {message: `${name} not found`, status: 404}
+    });
+  }
+  else {
+    res.send(users_with_name);
+  }
+  // let usernames = req.users.map(function(user) {
+  //   return {id: user.id, username: user.username};
+  // });
+  // res.send(usernames);
 });
 
 app.use(express.json());
