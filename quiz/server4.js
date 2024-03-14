6481f5bd8bb0b904722667fea47fab5c178edc56
Mo Barre <mbarre95@gmail.com>
2024-03-14 13:57:02 -0400

    Feb 12 updates

--- quiz/server4.js
+++ quiz/server4.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-var cors = require('cors');
+const cors = require('cors');
 const port = 8000;
 
 const readUsers = require('./readUsers');
@@ -13,7 +13,7 @@ fs.readFile(path.resolve(__dirname, '../data/users.json'), function(err, data) {
   console.log('reading file ... ');
   if(err) throw err;
   users = JSON.parse(data);
-})
+});
 
 const addMsgToRequest = function (req, res, next) {
   if(users) {
@@ -25,19 +25,18 @@ const addMsgToRequest = function (req, res, next) {
         error: {message: 'users not found', status: 404}
     });
   }
-  
-}
+};
 
 app.use(
   cors({origin: 'http://localhost:3000'})
 );
 app.use(addMsgToRequest);
-
-app.use('/read', readUsers);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/read', readUsers);
 app.use('/write', writeUsers);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
